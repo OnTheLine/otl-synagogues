1e2a4d40d221d1e9918881d63a141bdad22788d2
Jack Dougherty <jack.dougherty@trincoll.edu>
2017-11-02 17:07:26 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1okisdTeXeMByWt6rb7jgksr72vvUoWOJDFFcgx4gFVk';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1okisdTeXeMByWt6rb7jgksr72vvUoWOJDFFcgx4gFVk/edit#gid=0';
